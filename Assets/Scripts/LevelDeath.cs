0b867ae07b52c8465b1698cb967689e9a0297fe3
Colby Reinhart <colbyreinhart@gmail.com>
2023-03-27 21:16:48 -0400

    Completed tutorial episode 9

--- Assets/Scripts/LevelDeath.cs
+++ Assets/Scripts/LevelDeath.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LevelDeath : MonoBehaviour
 {
+    public GameObject youFell;
+    public GameObject levelAudio;
+    public GameObject fadeOut;
+
     void OnTriggerEnter()
     {
+        StartCoroutine(YouFellOff());
+    }
+
+    IEnumerator YouFellOff()
+    {
+        youFell.SetActive(true);
+        levelAudio.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(0);
     }
 }
